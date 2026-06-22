2f8b34b14a9e1e749b6fc8ad7328dd770fe99416
nehanimje03 <nehanimje03@gmail.com>
2026-06-22 20:18:43 +0530

    changes

--- products/APIs/product.py
+++ products/APIs/product.py
@@ -2,25 +2,26 @@ from ..views import *
 
 
 class AllProductsAPIView(APIView):
-
     def get(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
 
-        return Response({
+        response_data = {
             "status": True,
             "message": "All Products",
             "data": serializer.data
-        }, status=status.HTTP_200_OK)
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
     
 
 class PopularProductsAPIView(APIView):
     def get(self, request):
         products = Product.objects.filter(is_popular=True)
         serializer = ProductSerializer(products, many=True)
-
-        return Response({
+        
+        response_data = {
             "status": True,
             "message": "Popular Products",
             "data": serializer.data
-        }, status=status.HTTP_200_OK)
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
